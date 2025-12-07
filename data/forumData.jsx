cc89f6c81fc932bc51b9a986699a6155d5a304c2
Alison Haire <alihaire900@gmail.com>
2025-12-07 19:31:54 +1100

    Update forum data - 2025-12-07T03:31:54.003-05:00

---
+++ data/forumData.jsx
@@ -0,0 +1,26 @@
+export const forumData = [
+  {
+    title: 'It\'s time to ACT! Accumulation & the Treasury Ceiling',
+    href: 'https://forum.livepeer.org/t/3153',
+    author: '_By b3nnn (@b3nnn)_',
+    content: 'The onchain treasury was designed to provide sustainable public goods funding. It has supported many important and strategic contributions to the Livepeer Ecosystem. The AI SPE, Streamplace, Agent SPE and Cloud have all received funds and made import...',
+  },
+  {
+    title: 'Pre-proposal: IDOL - Improving Dex / Onchain Liquidity',
+    href: 'https://forum.livepeer.org/t/3151',
+    author: '_By b3nnn (@b3nnn)_',
+    content: 'TLDR We propose to address known UX issues and ease and costs to participate by increasing DEX liquidity. Arrakis offers an optimal solution for our specific needs, and we are requesting 250,000 LPT for deployment to a Uniswap v4 pool which will sign...',
+  },
+  {
+    title: 'Transformation SPE Release Notes',
+    href: 'https://forum.livepeer.org/t/3142',
+    author: '_By Mehrdad (@Mehrdad)_',
+    content: 'Release notes are a way to share work being completed by the Transformation SPE and it’s various contributors. Dive in and explore what has been happening and please reach out or reply with any questions and we will happily expand further.',
+  },
+  {
+    title: 'Transcoder Campaign: organic-node.eth',
+    href: 'https://forum.livepeer.org/t/1970',
+    author: '_By Ron (@ron)_',
+    content: 'Hello fellow video enthusiast and web3 supporters, Thanks for your time in reading my post. (organic-node.eth) Node has been active for about 6 months and everyday has been a great learning experience. My node has been highly reliable with 4 Orchestr...',
+  }
+];
